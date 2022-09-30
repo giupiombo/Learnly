b9f3536140024dd8c5cdd6bb998edea448f07f2b
Giulia Piombo <giulia.piombo01@gmail.com>
2022-09-30 14:47:10 -0500

    Login page validating from firebase

--- src/components/LoginPage/LoginDisplay.js
+++ src/components/LoginPage/LoginDisplay.js
@@ -1,23 +1,58 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../UI/Footer';
 import Header from '../UI/Header';
 import Login from './Login';
 
 const LoginDisplay = () => {
-  const [usersList, setUsersList] = useState([]);
-  //userList will be used to validate the user later
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch(
+        'https://react-http-bb74b-default-rtdb.firebaseio.com/accounts.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Invalid Account!');
+      }
+
+      const responseData = await response.json();
+
+      const accounts = [];
+
+      for (const key in responseData) {
+        accounts.push({
+          email: responseData[key].user.email,
+          password: responseData[key].user.password,
+          accountType: responseData[key].user.accountType,
+        });
+      }
+
+      setUsers(accounts);
+      setIsLoading(false);
+    };
+
+    fetchUsers().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
 
   const loginHandler = (email, password) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        {
-          email: email,
-          password: password,
-          id: Math.random().toString(),
-        },
-      ];
-    });
+    for (const user in users) {
+      if (users[user].email === email && users[user].password === password) {
+        if (users[user].accountType === 'student') {
+          navigate('/studentCourses');
+        } else {
+          navigate('/professorCourses');
+        }
+      }
+    }
   };
 
   return (
---
+++ src/components/Student/MyCourses.js
@@ -0,0 +1,5 @@
+const MyCourses = () => {
+  return <p>Student</p>;
+};
+
+export default MyCourses;
