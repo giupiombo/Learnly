3c7b848aebb4b74184fe1b943f3ab98fe1430afe
Giulia Piombo <giulia.piombo01@gmail.com>
2022-11-01 18:38:18 -0500

    Header changes

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,9 +1,29 @@
+import { useNavigate } from 'react-router-dom';
 import classes from './Header.module.css';
 
 const Header = () => {
+  let navigate = useNavigate();
+
+  const homeHandler = () => {
+    navigate('/');
+  };
+
+  const loginHandler = () => {
+    navigate('/login');
+  };
+
+  const createAccountHandler = () => {
+    navigate('/createAccount');
+  };
+
   return (
     <header className={classes.header}>
-      <h2>Learnly</h2>
+      <button className={classes.home} onClick={homeHandler}>
+        Learnly
+      </button>
+      <span></span>
+      <button onClick={loginHandler}>Login</button>
+      <button onClick={createAccountHandler}>Sign Up</button>
     </header>
   );
 };
