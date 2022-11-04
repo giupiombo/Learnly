9667a58daf38e8b46a37cee3b0d37460b8d7eb9f
cohlenburger2 <cohlenburger22@gmail.com>
2022-11-04 12:27:48 -0500

    beginning of create account front -> back

--- src/components/CreateAccountPage/CreateAccountDisplay.js
+++ src/components/CreateAccountPage/CreateAccountDisplay.js
@@ -1,15 +1,37 @@
 import Footer from '../Common/Footer';
 import Header from '../Common/Header';
 import CreateAccount from './CreateAccount';
+import { getFirestore, addDoc, serverTimestamp, collection } from 'firebase/firestore'
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth'
+
+const db = getFirestore()
+const auth = getAuth()
+const colRef = collection(db, 'users')
 
 const CreateAccountDisplay = () => {
   const createAccountHandler = async (
-    name,
-    email,
-    password,
-    reEnterPassword,
-    accountType
+    nameValue,
+    emailValue,
+    passwordValue,
+    reEnterPasswordValue,
+    accountTypeValue
   ) => {
+    createUserWithEmailAndPassword(auth, emailValue, passwordValue)
+        .then((cred) => {
+            sendEmailVerification(cred.user)
+            addDoc(colRef, {
+                email: emailValue,
+                password: passwordValue,
+                reEnterPassword: reEnterPasswordValue,
+                name: nameValue,
+                accountType: accountTypeValue,
+                createdAt: serverTimestamp() 
+            }) 
+            console.log('user created', cred.user)
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
     // await fetch(
     //   'https://react-http-bb74b-default-rtdb.firebaseio.com/accounts.json',
     //   {
---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBFVxs3fiRvkJgkuYeeHzSLXs9ZqAhjzps",
+    authDomain: "projectlab4-89008.firebaseapp.com",
+    databaseURL: "https://projectlab4-89008-default-rtdb.firebaseio.com",
+    projectId: "projectlab4-89008",
+    storageBucket: "projectlab4-89008.appspot.com",
+    messagingSenderId: "142241090923",
+    appId: "1:142241090923:web:efe61b801a97dbcb4bd02e",
+    measurementId: "G-2YJC9GWYXX"
+  };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
