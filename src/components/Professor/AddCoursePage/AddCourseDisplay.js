c09f16a6670f7bde24fc40a5299e1cde7f69604c
cohlenburger2 <cohlenburger22@gmail.com>
2022-11-30 14:42:03 -0600

    Update AddCourseDisplay.js

--- src/components/Professor/AddCoursePage/AddCourseDisplay.js
+++ src/components/Professor/AddCoursePage/AddCourseDisplay.js
@@ -3,37 +3,19 @@ import HeaderProfessor from '../HeaderProfessor';
 import AddCourse from './AddCourse';
 import {
   getFirestore,
-  serverTimestamp,
   setDoc,
   doc,
   collection,
-  addDoc
 } from 'firebase/firestore';
 
 const db = getFirestore();
 
 const AddCourseDisplay = () => {
   const addCourseHandler = async (
-    // titleValue,
-    // descriptionValue,
-    // categoryValue,
-    // priceValue,
-    // videoValue,
-    // imageValue,
-    // name
     courseData
     ) => {
       const coursesRef = doc(collection(db, "courses"));
           await setDoc(coursesRef, courseData
-          // {
-            // title: titleValue,
-            // description: descriptionValue,
-            // category: categoryValue,
-            // price: priceValue,
-            // video: videoValue,
-            // image: imageValue,
-            // author: name,
-        // }
         )
         console.log(courseData)
         console.log('course added');
