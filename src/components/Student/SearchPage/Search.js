555e1f1b46fa471b49ce4c2c6551caf46b149dbf
Giulia Piombo <giulia.piombo01@gmail.com>
2022-12-02 10:59:36 -0600

    search course

--- src/components/Student/SearchPage/Search.js
+++ src/components/Student/SearchPage/Search.js
@@ -1,14 +1,32 @@
 import { useContext } from 'react';
 import SearchCourseContext from '../../../context/search-course-context';
+import CourseItem from '../../UI/CourseItem';
+import HeaderStudent from '../HeaderStudent';
+import Footer from '../../Common/Footer';
+import classes from './Search.module.css';
 
 const Search = () => {
   const { searchCourses } = useContext(SearchCourseContext);
+  const courseList = searchCourses.map((course) => (
+    <CourseItem
+      id={course.id}
+      title={course.title}
+      price={course.price}
+      video={course.video}
+      author={course.author}
+      description={course.description}
+      category={course.category}
+      image={course.image}
+    />
+  ));
 
   return (
-    <>
-      <h1>search</h1>
-      {console.log(searchCourses)}
-    </>
+    <div className={classes.search}>
+      <HeaderStudent />
+      <h1>Searched Course</h1>
+      {courseList}
+      <Footer />
+    </div>
   );
 };
 
